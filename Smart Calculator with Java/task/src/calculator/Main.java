6595417ddfd9b95859ddb45274be1cd8fa925e30
Ahmad Fajar Prasetiyo <me@ahmadfajar.com>
2024-02-04 20:16:34 +0700

    Stage 4/8: Add subtractions

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -25,19 +25,42 @@ public class Main {
                 continue;
             }
 
+            line = line.replaceAll("\\+", "");
+
+            while (line.contains("  ") || line.contains("--")) {
+                line = line.replaceAll(" {2}", " ");
+                line = line.replaceAll("--", "");
+            }
+
             String[] splitLine = line.split(" ");
             if (splitLine.length == 1) {
                 System.out.println(splitLine[0]);
             } else {
-                int sum = 0;
-
-                for (String integerString : splitLine) {
-                    sum = sum + Integer.parseInt(integerString);
-                }
+                int sum = getSum(splitLine);
                 System.out.println(sum);
             }
         } while (true);
 
         System.out.println("Bye!");
+    }
+
+    private static int getSum(String[] splitLine) {
+        int sum = 0;
+        boolean nextNegative = false;
+        for (String integerString : splitLine) {
+            if (integerString.equals("-")) {
+                nextNegative = true;
+                continue;
+            }
+
+            if (nextNegative) {
+                sum = sum - Integer.parseInt(integerString);
+                nextNegative = false;
+            } else {
+                sum = sum + Integer.parseInt(integerString);
+            }
+
+        }
+        return sum;
     }
 }
