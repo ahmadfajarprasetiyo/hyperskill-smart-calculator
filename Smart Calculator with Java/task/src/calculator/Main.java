e269fb6dcea7ee04baabbfcbf0c53d5267a3db42
Ahmad Fajar Prasetiyo <me@ahmadfajar.com>
2024-02-04 19:53:12 +0700

    Stage 1/8: 2+2

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -7,5 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.print(num1 + num2);
     }
 }
