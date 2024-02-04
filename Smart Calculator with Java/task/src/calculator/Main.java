61a906d50a9e31ad4cfe9bcfc44f0f00efb3dc8d
Ahmad Fajar Prasetiyo <me@ahmadfajar.com>
2024-02-04 19:59:41 +0700

    Stage 2/8: 2+2+

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -5,10 +5,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        final String EXIT_CMD = "/exit";
+
         Scanner scanner = new Scanner(System.in);
-        // put your code here
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        System.out.print(num1 + num2);
+
+        do {
+            String line = scanner.nextLine();
+            if (line.equals(EXIT_CMD)) {
+                break;
+            }
+
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            String[] splitLine = line.split(" ");
+            if (splitLine.length == 1) {
+                System.out.println(splitLine[0]);
+            } else {
+                System.out.print(Integer.parseInt(splitLine[0]) + Integer.parseInt(splitLine[1]));
+            }
+        } while (true);
+
+        System.out.println("Bye!");
     }
 }
